a952e512c5c7e4c504732dbb0246ffc8bd819859
Viktor Schneider <info@vjs.io>
2021-09-29 00:47:42 +0200

    add customization options

--- main.cpp
+++ main.cpp
@@ -66,71 +66,141 @@ void copy_metadata(Exiv2::Image &src, Exiv2::Image &dst) {
 }
 
 extern "C" {
-int run(const char *infile, const char *outfile) {
+
+// relative to short side of final image
+// polaroid: 1/15 padding left, right and top. 1/4 bottom
+// source:
+// https://support.polaroid.com/hc/en-us/articles/115012363647-What-are-Polaroid-photo-dimensions-
+
+int run(const char *infile, const char *outfile, int reference_length = 900,
+        int left_pad_length = 60, int top_pad_length = 60,
+        int right_pad_length = 60, int bottom_pad_length = 225,
+        bool fill_squared = false) {
+    std::cout << "reference length=" << reference_length
+              << " left pad=" << left_pad_length
+              << " top pad=" << top_pad_length
+              << " right pad=" << right_pad_length
+              << " bottom pad=" << bottom_pad_length
+              << " squared=" << fill_squared << std::endl;
+
     try {
-    std::cout << "loading " << infile << std::endl;
-    std::ifstream img_file(infile);
+        std::cout << "loading " << infile << std::endl;
+        std::ifstream img_file(infile);
 
-    gil::rgb8_image_t img;
-    gil::read_image(img_file, img, gil::jpeg_tag());
+        gil::rgb8_image_t img;
+        gil::read_image(img_file, img, gil::jpeg_tag());
 
-    Exiv2::XmpParser::initialize();
-    ::atexit(Exiv2::XmpParser::terminate);
+        Exiv2::XmpParser::initialize();
+        ::atexit(Exiv2::XmpParser::terminate);
 
-    auto in_exif = Exiv2::ImageFactory::open(infile, false);
-    if (!in_exif.get()) {
-        std::cerr << "failed to load exif from " << infile << std::endl;
-        return -1;
-    }
-    in_exif->readMetadata();
+        auto in_exif = Exiv2::ImageFactory::open(infile, false);
+        if (!in_exif.get()) {
+            std::cerr << "failed to load exif from " << infile << std::endl;
+            return -1;
+        }
+        in_exif->readMetadata();
 
-    // relative to short side of final image
-    // polaroid: 1/15 padding left, right and top. 1/4 bottom
-    // source:
-    // https://support.polaroid.com/hc/en-us/articles/115012363647-What-are-Polaroid-photo-dimensions-
+        auto exif_orientation = Exiv2::orientation(in_exif->exifData());
+        int orientation = 1;
+        if (exif_orientation != in_exif->exifData().end()) {
+            orientation = exif_orientation->toLong();
+            std::cout << "orientation: " << orientation << std::endl;
+        }
 
-    auto short_side = std::min(img.height(), img.width());
+        // rotate padding with respect to exif orientation
+        switch (orientation) {
+        case 8: // 0th row is left; 0th column is bottom
+            std::swap(right_pad_length, left_pad_length);
+            std::swap(top_pad_length, right_pad_length);
+            std::swap(bottom_pad_length, left_pad_length);
+            break;
+        case 7: // 0th row is right; 0th column is bottom
+            std::swap(top_pad_length, right_pad_length);
+            std::swap(bottom_pad_length, left_pad_length);
+            break;
+        case 6: // 0th row is right; 0th column is top
+            std::swap(left_pad_length, right_pad_length);
+            std::swap(top_pad_length, left_pad_length);
+            std::swap(bottom_pad_length, right_pad_length);
+            break;
+        case 5: // 0th row is left; 0th column is top
+            std::swap(top_pad_length, left_pad_length);
+            std::swap(bottom_pad_length, right_pad_length);
+            break;
+        case 4: // 0th row is bottom; 0th column is left
+            std::swap(top_pad_length, bottom_pad_length);
+            break;
+        case 3: // 0th row is bottom; 0th column is right
+            std::swap(top_pad_length, bottom_pad_length);
+            std::swap(left_pad_length, right_pad_length);
+            break;
+        case 2: // 0th row is top; 0th column is right
+            std::swap(left_pad_length, right_pad_length);
+            break;
+        case 1: // 0th row is top; 0th column is left
+        default: break;
+        }
 
-    int rpad = short_side / 15, lpad = short_side / 15, tpad = short_side / 15,
-        bpad = short_side / 15;
+        using coord_t = decltype(img)::coord_t;
+        coord_t rpad = 0, lpad = 0, tpad = 0, bpad = 0;
 
-    auto exif_orientation = Exiv2::orientation(in_exif->exifData());
-    int orientation = 1;
-    if (exif_orientation != in_exif->exifData().end()) {
-        orientation = exif_orientation->toLong();
-        std::cout << "orientation: " << orientation << std::endl;
-    }
+        coord_t width = img.width();
+        coord_t height = img.height();
 
-    switch (orientation) {
-    case 3:
-    case 4: tpad = short_side / 4; break; // 0th row is bottom
+        // fill short side for squared image
+        if (fill_squared) {
+            if (img.height() >= img.width()) {
+                auto pad = (img.height() - img.width()) / 2;
+                lpad += pad;
+                rpad += pad;
+                width = img.width() + 2 * pad;
+            } else {
+                auto pad = (img.width() - img.height()) / 2;
+                tpad += pad;
+                bpad += pad;
+                height = img.height() + 2 * pad;
+            }
+        }
 
-    case 5:
-    case 6: rpad = short_side / 4; break; // 0th column is top
+        bool is_width_short_side =
+            (reference_length * width /
+             (reference_length - left_pad_length - right_pad_length)) <=
+            (reference_length * height /
+             (reference_length - bottom_pad_length - top_pad_length));
 
-    case 7:
-    case 8: lpad = short_side / 4; break; // 0th column is bottom
+        coord_t short_side_length;
+        coord_t short_side;
+        if (is_width_short_side) {
+            short_side = width;
+            short_side_length =
+                reference_length - left_pad_length - right_pad_length;
+        } else {
+            short_side = height;
+            short_side_length =
+                reference_length - top_pad_length - bottom_pad_length;
+        }
 
-    case 1:
-    case 2:
-    default: bpad = short_side / 4; break; // 0th row is top
-    }
+        lpad += left_pad_length * short_side / short_side_length;
+        rpad += right_pad_length * short_side / short_side_length;
+        tpad += top_pad_length * short_side / short_side_length;
+        bpad += bottom_pad_length * short_side / short_side_length;
 
-    auto v = padded_image(gil::view(img), tpad, lpad, bpad, rpad);
+        auto v = padded_image(gil::view(img), tpad, lpad, bpad, rpad);
 
-    std::ofstream img_file_out(outfile);
+        std::ofstream img_file_out(outfile);
 
-    std::cout << "created outstream" << std::endl;
-    gil::write_view(img_file_out, v, gil::image_write_info<gil::jpeg_tag>(100));
-    img_file_out.close();
+        std::cout << "created outstream" << std::endl;
+        gil::write_view(img_file_out, v,
+                        gil::image_write_info<gil::jpeg_tag>(100));
+        img_file_out.close();
 
-    auto out_exif = Exiv2::ImageFactory::open(outfile, false);
-    if (!out_exif.get()) {
-        std::cerr << "failed to load exif from " << outfile << std::endl;
-        return -1;
-    }
-    copy_metadata(*in_exif, *out_exif);
-    out_exif->writeMetadata();
+        auto out_exif = Exiv2::ImageFactory::open(outfile, false);
+        if (!out_exif.get()) {
+            std::cerr << "failed to load exif from " << outfile << std::endl;
+            return -1;
+        }
+        copy_metadata(*in_exif, *out_exif);
+        out_exif->writeMetadata();
 
     } catch (std::exception &err) {
         std::cerr << err.what() << std::endl;
